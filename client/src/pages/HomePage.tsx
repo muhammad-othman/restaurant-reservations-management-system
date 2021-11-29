47ea95f82aa27a49cbe941366cb8ee455b663be0
Muhammad Othman <muhammad.oth91@gmail.com>
2021-11-30 01:44:55 +0200

    add tables grid to homePage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Row } from 'react-bootstrap';
 import { useNavigate, useHref } from 'react-router';
+import GridCell from '../components/GridCell';
 import AuthContext from '../contexts/AuthContext';
 import LoadingContext from '../contexts/LoadingContext';
 import RestaurantContext from '../contexts/RestaurantContext';
+import { generateGridArray } from '../utils/functions';
 
 const HomePage = () => {
   const { logout, currentUser } = useContext(AuthContext);
@@ -16,11 +18,11 @@ const HomePage = () => {
 
   return (
     <div>
-      <Button className="login-form-button mt-3" variant="primary" onClick={logout}>
-        Logout
-      </Button>
-      <pre>{JSON.stringify(userRestaurant, null, 4)}</pre>
-
+      <Row xs={10}>
+        {generateGridArray(userRestaurant.tables).map((table, index) => (
+          <GridCell key={index + 1} table={table} index={index + 1} />
+        ))}
+      </Row>
     </div>
   )
 };
---
+++ client/src/utils/functions.ts
@@ -0,0 +1,9 @@
+import { ITable } from "../types";
+
+export const generateGridArray = (tables: Array<ITable>, size: number = 150): Array<ITable> => {
+  const array: Array<ITable> = [];
+  for (let i = 1; i <= size; i++) {
+    array.push(tables.find(e => e.index === i));
+  }
+  return array;
+};
